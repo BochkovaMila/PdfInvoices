e6d5c437aa30926f9842f9265607b24c5dc05d7b
Mila Bochkova <milabochkova03@gmail.com>
2024-10-18 12:38:41 +0300

    add Login Form Submission

--- src/main/java/com/milabochkova/web/WebsiteController.java
+++ src/main/java/com/milabochkova/web/WebsiteController.java
@@ -1,8 +1,12 @@
 package com.milabochkova.web;
 
+import com.milabochkova.web.forms.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
@@ -15,6 +19,24 @@ public class WebsiteController {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "login.html";
+        }
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
     }
 
 }
