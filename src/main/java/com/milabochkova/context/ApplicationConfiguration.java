ea3787a070114a1a3a0ac592cb4c785a7a9435b0
Mila Bochkova <milabochkova03@gmail.com>
2024-10-21 14:00:35 +0300

    add H2 Database

--- src/main/java/com/milabochkova/context/ApplicationConfiguration.java
+++ src/main/java/com/milabochkova/context/ApplicationConfiguration.java
@@ -2,12 +2,15 @@ package com.milabochkova.context;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.milabochkova.ApplicationLauncher;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.*;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
@@ -20,6 +23,15 @@ public class ApplicationConfiguration {
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:~/MyFirstH2Database;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        ds.setUser("sa");
+        ds.setPassword("sa");
+        return ds;
     }
 
     @Bean
